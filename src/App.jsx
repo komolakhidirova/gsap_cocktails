c96447addda0d773eacbd7cd19bfb7e45e401d25
Komola Khidirova <komolakhidirova@gmail.com>
2025-06-30 18:42:38 +0500

    hero layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
@@ -10,6 +11,8 @@ const App = () => {
 	return (
 		<main>
 			<Navbar />
+			<Hero />
+			<div className='h-dvh bg-black'></div>
 		</main>
 	)
 }
