c3d5364873b8035b47474e546b59d508baec3528
Komola Khidirova <komolakhidirova@gmail.com>
2025-07-01 15:38:48 +0500

    art

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import About from './components/About'
+import Art from './components/Art'
 import Cocktails from './components/Cocktails'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
@@ -16,6 +17,7 @@ const App = () => {
 			<Hero />
 			<Cocktails />
 			<About />
+			<Art />
 		</main>
 	)
 }
