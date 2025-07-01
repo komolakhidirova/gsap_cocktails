5d96c03f2162dbbe0e0e3c0cedb9096f512c7d14
Komola Khidirova <komolakhidirova@gmail.com>
2025-07-01 16:10:53 +0500

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './components/About'
 import Art from './components/Art'
 import Cocktails from './components/Cocktails'
 import Hero from './components/Hero'
+import Menu from './components/Menu'
 import Navbar from './components/Navbar'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
@@ -18,6 +19,7 @@ const App = () => {
 			<Cocktails />
 			<About />
 			<Art />
+			<Menu />
 		</main>
 	)
 }
